5fd7bb18e899cb980f56db6d2117bb48923f1a40
Edward Zhang <edward@zhang.nz>
2017-08-15 22:57:40 +1200

    Initial commit

---
+++ src/zhang/nz/mathsAid/Controller.java
@@ -0,0 +1,11 @@
+package zhang.nz.mathsAid;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controller {
+    @FXML private Button playbtn;
+    @FXML private Button deletebtn;
+    @FXML private Button createbtn;
+    @FXML private ListView creationlist;
+}
